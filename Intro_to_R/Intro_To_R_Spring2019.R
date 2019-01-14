d3fc60c6fe7cb9474be85daa1de3b0c14f8a2b86
Richard Johansen <richard.johansen@uc.edu>
2019-01-14 12:10:14 -0500

    Updated to importing data frames

--- Intro_to_R/Intro_To_R_Spring2019.R
+++ Intro_to_R/Intro_To_R_Spring2019.R
@@ -10,7 +10,7 @@
 #Twitter: @DataVizJohansen
 
 
-### R Interface Basics--------------------------------------------------------------
+### R Basics--------------------------------------------------------------
 ## R as a Calculator
 #simple to complex equations
 1+2
@@ -47,6 +47,10 @@ help(mean)
 #HINT: if you can't remember exactly what function you are looking for, Use Tab
 me "Tab"
 
+#Not sure what its called 
+#Try a fuzzy search
+apropos("mea") 
+
 ## Creating a list of numbers
 list <- c(1,2,3,4,5,6,7,8,9,10)
 
@@ -59,14 +63,39 @@ list <- 1:100
 
 #To accomplish this goal we will use the sequence or seq() function
 #The first thing I do when using a new functions is to look at the documentation.
-#HINT: Use the ? to find R documentation on an object
+#Use the ? to find R documentation on an object
 ?seq()
+
+#HINT!!: Scroll to the bottom of the help page for workable examples.
 
 # Task #1:
 #Using this function how would you create a sequence from 1 to 100? 
 seq(1,10)
 
-# TASK 1B #
-#Create a sequence of numbers from 1 to 100 by 2 and save it to an object called Seq1
-Seq <- seq(1, 100, by =2) #define step by value
+# TASK 1B
+#Create a sequence of numbers from 1 to 100 by 2 called my_seq
+my_seq <- seq(1, 100, by =2) #define step by value
 
+##Plotting using base plot
+#We will come back to plotting
+#But I want to show how quick and easy it is to plot in R
+
+#Plot our sequence from Task 1B
+plot(my_seq)
+#Create a histogram of 100 random normal values
+hist(rnorm(100))
+
+### Working with Data ----------------------------------------------------------------
+# Vectors: vectors (a row of numbers, also called arrays –1-dimensional)
+# Matrices: Matrices are nothing more than 2-dimensional vectors.
+# Data Frames: A data frame is a matrix with names above the columns 
+# Lists: The main advantage of lists is that the “columns” or collection of vectors
+  #don’t have to be of the same length, unlike matrices and data frames.
+
+## Most of the time you will be working with Data frames
+#Additionally, these data sets will be from else where so you need to import them
+
+## Importing data 
+read.csv("C:/myfolder/mydata/data.csv")
+readxl::read_xlsx("C:/myfolder/mydata/data.xlsx")
+mtcars  <- mtcars
