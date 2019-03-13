4e7b22ac37de88ab8978fc4be2e092564257ee90
Richard Johansen <richard.johansen@uc.edu>
2019-03-13 10:21:06 -0400

    Initial Commit

---
+++ Mapping_in_R/Mapping_in_R.R
@@ -0,0 +1,17 @@
+#########################
+###   Mapping in R    ###
+###  Richard Johansen ###
+###  April 18th 2019  ###
+#########################
+
+#Install & Load Packages
+
+#Mapping with baseplot
+
+#Mapping with ggplot
+
+#Interactive Mapping with Leaflet
+
+#Tmap
+
+#Others?
