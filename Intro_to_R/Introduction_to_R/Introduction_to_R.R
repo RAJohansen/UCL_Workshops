ee7a010b372e1f93ba06ea0027e4a482b5598f0a
Richard Johansen <richard.johansen@uc.edu>
2019-10-29 14:58:29 -0400

    On Section III The tidyverse

--- Intro_to_R/Introduction_to_R/Introduction_to_R.R
+++ Intro_to_R/Introduction_to_R/Introduction_to_R.R
@@ -14,20 +14,32 @@ browseURL("https://github.com/RAJohansen")
 browseURL("https://twitter.com/johansen_phd")
 
 ### Section I: Coding Basics --------------------------------------------------------------
+#### 1. R as a Calculator/Working with Mathematical Operators ####
 
-#### 1.  R as a Calculator ####
+# Addition (+)
+1+2 
 
-#simple to complex equations 
-1+2
-[1] 3
+# Subtraction (-)
+15-8
 
+#Multiplication (*)
+12*20
+
+#Division (/)
+20/5
+
+#Equation 
 sin(pi*15)/100
-[1] 5.389623e-17
+
 
 #### 2.  Objects and Assignment Statements ####
+# R stores values and objects so they can be reused throughout an equation or script
 
+# Object Assignment
 x <- 1+2
 x
+
+#Working with multiple objects
 y <- x +1
 y
 
@@ -45,7 +57,7 @@ y
 #periods "i.use.periods"
 #combinations "iCant_decide.which_toUse"
 
-#### Using Comments ####
+#### 4. Using Comments ####
 #Using the # sign to create a comment
 #Comments are essential for reproducible code
 #Your most likely collaborator is future you!!
@@ -68,17 +80,6 @@ area <- pi*r^2 #formula for calculating the area of a circle
 #This still is somewhat intuitive but what if you're making your own formula?
 # Do not use "magic" numbers
 #Every number standalone number should have a description Example p above.
-
-#### 4. Getting Help in R ####
-help(mean)
-#HINT: if you can't remember exactly what function you are looking for, Use Tab
-me #press tab
-
-#Not sure what its called 
-#Try a fuzzy search
-apropos("mea") 
-
-
 
 #### 5. Creating a series of numbers ####  
 #Lets say you want to create a series of numbers from 1-10 and call the object "series"
@@ -103,16 +104,26 @@ series <- 1:100
 #The first thing I do when using a new functions is to look at the documentation.
 #Use the ? to find R documentation on an object
 ?seq()
-
 #HINT!!: Scroll to the bottom of the help page for workable examples.
 
-# Task #1:
+
+### Getting Help in R
+help(mean)
+#HINT: if you can't remember exactly what function you are looking for, Use Tab
+me #press tab
+
+#Not sure what its called 
+#Try a fuzzy search
+apropos("mea") 
+
+#### Section I Tasks ####
+# Task 1A:
 # Work with your neighbor or by yourself and explore the documentation
 
 #Create a sequence from 1 to 10 using the seq() function
 seq(1,10)
 
-# TASK 1B
+# Task 1B
 #Create a sequence of numbers from 50 to 150 by 3
 seq(50, 150, by =3) #define step by value
 
@@ -120,7 +131,8 @@ seq(50, 150, by =3) #define step by value
 #Create a sequence of numbers from 25 to 0 decreasing by 5 and save it an object called "my_seq"
 my_seq <- seq(25, 0, by =-5) #define step by value
 
-
+####---------------------------------####
+####---------------------------------####
 ### Section II: Creating, Importing and Exporting data ----------------------------------------------------------------
 # Vectors: vectors (a row of numbers, also called 1-dimensional arrays)
 # Matrices: Matrices are nothing more than 2-dimensional vectors.
@@ -162,7 +174,7 @@ View(company)
 # Lets save our company data to our computer
 write.csv(my_seq, "C:/temp/data.csv")
 
-#### 3, Importing data ####
+#### 3. Importing data ####
 # Just like saving, there are many ways to import data but we will stick with csv for this exercise
 
 # Importing our comapny data from our computer using read.csv and call it company_data
@@ -170,68 +182,140 @@ write.csv(my_seq, "C:/temp/data.csv")
 # Alternatively, you can use the import dataset button in the top right (but using code is always beter)
 
 company_data <- read.csv("C:/temp/data.csv")
-df <- readxl::read_xlsx("C:/myfolder/mydata/data.xlsx")
+
+# Example for importing with Excel
+df <- readxl::read_xlsx("C:/myfolder/mydata/fake_data.xlsx")
 
 
+####---------------------------------####
+####---------------------------------####
+### Section III: Exploring the Tidyverse!----------------------------------------------------------------
 
-### Section III: Exploring & Manipulating data----------------------------------------------------------------
-Look at:
-  R_Tidyr_Basics
-  R_Stringr_Basics
-  R_Transformation_Basics
-  Introduction_to_the_Tidyverse
+# Install and Load the tidyverse package
+require("tidyverse")
+require("gapminder")
 
+#Explore the Tidyverse 
+browseURL("https://www.tidyverse.org/")
 
+# R packages must be installed once but loaded everytime
+# Using require is a nice way to make sure every script has the packages required
+#Combines
+install.packages() 
+library()
 
+#### 1. Basic Data Exploration ####
+#For this section we will use the gapminder data set thats contained in Base R
 
-#Lets explore data using a data set thats contained in R
-mtcars  <- mtcars
+#Lets assign this to gapminder
+gapminder <- gapminder
 
 #View our table
 #click the df object under the Data window or
-View(mtcars)
+View(gapminder)
 
 # Lists the variables 
-names(mtcars)
+names(gapminder)
 
 #Lets look at the structure of the data
-str(mtcars)
+str(gapminder)
 #This will become very useful when we visualize the data
 #Make sure your variables are in the appropiate format!!
 
 ## Statistical summary of the data
-summary(mtcars)
+summary(gapminder)
 
 ## Finding values from out data table
 # Lets look at column 2 
-mtcars[,2]
+gapminder[,2]
 
 #Lets look at row 5
-mtcars[5,]
+gapminder[5,]
 
 #What value is in row 5 column 3?
-mtcars[5,3]
+gapminder[5,3]
 
 # Based on this idea, we can make more complicated searches
-#Lets take the first ten mtcars and look at their MPG (1), HP(4), and qsec(7)
-mtcars[1:10,c(1,4,7)]
+#Lets take the first ten observations and look at the variables:
+# Country (1), Continent(2), Year (3), and population (5)
+gapminder[1:10,c(1:3, 5)]
 
-# What if we want to know the max mpg
-max(mtcars$mpg)
-#Lets find the row number of the vehicle with the highest mpg
-which(mtcars== max(mtcars$mpg))
+# What if we want to know the highest gpdPercap
+max(gapminder$gdpPercap)
+
+#Lets find the row number of the country with the highest gpdpercap
+which(gapminder== max(gapminder$pop))
+
 #Then show me all columns for row 20
-df[20,]
+mtcars[20,]
 
-#**More efficiently
-df[which.max(mtcars$mpg),]
 
-#TASK 2
+#### 2. The filter verb ####
+#The filter verb is used to look at a subset of a data set
+# Typically you combine filter with a pipe %>%
+
+mtcars %>% 
+  filter(year == 2007)
+
+gapminder %>% 
+  filter(country == "United States")
+
+#Multiple Conditions
+gapminder %>% 
+  filter(year == 2007, country == "United States")
+
+### The arrange verb ----------------------------------
+#Used for sorting data by ascending or descending condition
+
+#Default is lowest or smallest value
+gapminder %>% 
+  arrange(gdpPercap)
+
+# To sort by descending value
+gapminder %>% 
+  arrange(desc(gdpPercap))
+
+### Combining verbs
+gapminder %>% 
+  filter(year == 2007) %>% 
+  arrange(gdpPercap)
+
+### The mutate verb ----------------------------------
+#Change or Add variables to a data set
+
+#Change a variable
+gapminder %>% 
+  mutate(pop = pop/1000000)
+
+#Add a new variable called gdp
+gapminder %>% 
+  mutate(gdp = gdpPercap * pop)
+
+### Combine all three verbs 
+gapminder %>% 
+  mutate(gdp = gdpPercap * pop) %>% 
+  filter(year == 2007) %>% 
+  arrange(desc(gdp))
+
+Look at:
+  R_Tidyr_Basics
+R_Stringr_Basics
+R_Transformation_Basics
+Introduction_to_the_Tidyverse
+
+
+
+
+
+
+#### Section II Tasks ####
 #find the fastest car with 6 cylinders
 mtcars_6 <- mtcars[which(mtcars$cyl== 6),]
 mtcars_6[which.min(mtcars_6$qsec),]
 
 
+####---------------------------------####
+####---------------------------------####
 ### Section IV: Programming with R ####
 Look at:
   R_Interations_Purrr_Basics
